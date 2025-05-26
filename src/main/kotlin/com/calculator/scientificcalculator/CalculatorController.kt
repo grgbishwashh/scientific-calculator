4c3b74cab2df790efdbc5aa5b8e9532c5aeffb3a
grgbishwashh <grgbishwashh@gmail.com>
2025-05-26 21:28:13 +0900

    Initial commit

---
+++ src/main/kotlin/com/calculator/scientificcalculator/CalculatorController.kt
@@ -0,0 +1,4 @@
+package com.calculator.scientificcalculator
+
+class CalculatorController {
+}
---
+++ src/test/kotlin/com/calculator/scientificcalculator/ScientificCalculatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.calculator.scientificcalculator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScientificCalculatorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
